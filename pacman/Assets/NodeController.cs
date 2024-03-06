bbc1610cc8dc945efb57c4e576824810c3d8aa45
A-Higley <114099322+A-Higley@users.noreply.github.com>
2024-03-06 08:59:40 -0800

    FIXED
    Fixed the movement, the four problem nodes, and the rigidbody

--- pacman/Assets/NodeController.cs
+++ pacman/Assets/NodeController.cs
@@ -18,10 +18,25 @@ public class NodeController : MonoBehaviour
     public bool isWarpRightNode = false;
     public bool isWarpLeftNode = false;
 
+    //If the node contains a pellet when the game starts
+    public bool isPelletNode = false;
+    //If the node still has a pellet
+    public bool hasPellet = false;
+
+    public SpriteRenderer pelletSprite;
+
+
 
     // Start is called before the first frame update
     void Awake()
     {
+        if (transform.childCount > 0)
+        {
+            hasPellet = true;
+            isPelletNode = true;
+            pelletSprite = GetComponentInChildren<SpriteRenderer>();
+        }
+
         RaycastHit2D[] hitsDown;
         //shoot ray cast (line) going down (because of the negative)
         hitsDown = Physics2D.RaycastAll(transform.position, -Vector2.up);
@@ -30,7 +45,7 @@ public class NodeController : MonoBehaviour
         for (int i = 0; i < hitsDown.Length; i++)
         {
             float distance = Mathf.Abs(hitsDown[i].point.y - transform.position.y);
-            if (distance < 0.4f)
+            if (distance < 0.4f && hitsDown[i].collider.tag == "node")
             {
                 canMoveDown = true;
                 nodeDown = hitsDown[i].collider.gameObject;
@@ -45,7 +60,7 @@ public class NodeController : MonoBehaviour
         for (int i = 0; i < hitsUp.Length; i++)
         {
             float distance = Mathf.Abs(hitsUp[i].point.y - transform.position.y);
-            if (distance < 0.4f)
+            if (distance < 0.4f && hitsUp[i].collider.tag == "node")
             {
                 canMoveUp = true;
                 nodeUp = hitsUp[i].collider.gameObject;
@@ -60,7 +75,7 @@ public class NodeController : MonoBehaviour
         for (int i = 0; i < hitsRight.Length; i++)
         {
             float distance = Mathf.Abs(hitsRight[i].point.x - transform.position.x);
-            if (distance < 0.4f)
+            if (distance < 0.4f && hitsRight[i].collider.tag == "node")
             {
                 canMoveRight = true;
                 nodeRight = hitsRight[i].collider.gameObject;
@@ -75,7 +90,7 @@ public class NodeController : MonoBehaviour
         for (int i = 0; i < hitsLeft.Length; i++)
         {
             float distance = Mathf.Abs(hitsLeft[i].point.x - transform.position.x);
-            if (distance < 0.4f)
+            if (distance < 0.4f && hitsLeft[i].collider.tag == "node")
             {
                 canMoveLeft = true;
                 nodeLeft = hitsLeft[i].collider.gameObject;
@@ -110,6 +125,15 @@ public class NodeController : MonoBehaviour
         else
         {
             return null;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && isPelletNode)
+        {
+            hasPellet = false;
+            pelletSprite.enabled = false;
         }
     }
 }
